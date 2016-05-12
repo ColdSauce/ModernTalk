602cbffed4169df88e6c32178c494404911edcb8
Shreyas <skraman1999@gmail.com>
2016-05-12 12:12:08 -0700

    Add on click event

--- src/main/scala/me/shreyasr/talk/App.scala
+++ src/main/scala/me/shreyasr/talk/App.scala
@@ -5,11 +5,17 @@ import org.scalajs.dom
 import dom.document
 
 import scala.scalajs.js.JSApp
+import scala.scalajs.js.annotation.JSExport
 
 object App extends JSApp {
 
   def main(): Unit = {
     appendPar(document.body, "Hello world!")
+  }
+
+  @JSExport
+  def onButtonClick(): Unit = {
+    appendPar(document.body, "You clicked the button!")
   }
 
   def appendPar(targetNode: scalajs.dom.Node, text: String): Unit = {
